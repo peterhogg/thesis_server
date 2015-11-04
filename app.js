f130ae6da05c2964412212af0f89aa7ee51796de
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-11-04 17:41:35 -0500

    fixed typo

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 
 io.on('connection', function(socket){
 	console.log('Socket Connected');
-	socket.onon("vote" ,function(data){
+	socket.on("vote" ,function(data){
     	console.log('Vote message received');
 	});
     
