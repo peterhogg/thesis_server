8e60c032a2481fce5526919c22de06f55d8d23ea
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-11-05 14:06:46 -0500

    added getter and setter functions to server data

--- app.js
+++ app.js
@@ -5,12 +5,57 @@ var server = require('http').createServer(app);
 var port = process.env.PORT || 5000
 var io = require('socket.io')(port);
 
+//Dummy Data
+var pollData = {
+	0 : {
+		question : "blah blah",
+		votes    : 2
+	},
+	1 : {
+		question : "something something",
+		votes    : 1
+	}
+};
+
 console.log("App is running at port " + port);
 
 io.on('connection', function(socket){
 	console.log('Socket Connected');
 	socket.emit("newVote",{"dummy": "data"});
 	socket.on("vote" ,function(data){
+		pollData =vote(pollData, data.index);
     	console.log('Vote message received');
 	});
 });
+
+//Get the amount of poll questions in the data
+function getSize(data){
+	return Object.keys(data).length;
+}
+
+//Gets the question at an index
+function getQuestion(data, index){
+	return data[index].question;
+}
+
+//Gets the votes at an index
+function getVotes(data, index){
+	return data[index].votes;
+}
+
+//Adds a poll option to the data
+function add(data,newQuestion){
+	var index = getSize(data);
+	data[index] = {
+					question : newQuestion,
+					votes    : 0
+				  };
+	return data;
+}
+//Votes for a question given its index
+function vote(data, index){
+	votes = getVotes(data,index);
+	votes ++;
+	data[index].votes = votes;
+	return data;
+}
