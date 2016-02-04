05a385d44f9b2ec004a40018b8ef866e7bd7305f
Peter Hoggarth <hoggarth.peter@gmail.com>
2016-02-04 00:46:54 -0500

    added reveal code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Setup basic express server
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
 var server = require('http').createServer(app);
 var port = process.env.PORT || 5000
 var io = require('socket.io')(server);
